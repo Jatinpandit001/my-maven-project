6df9fe7ddd2183a63d58285089a0d024460b4104
AdeebHaider137 <kaxmiadeeb@gmail.com>
2025-02-02 23:49:17 +0530

    Initial Maven project setup

---
+++ src/main/java/131/App.java
@@ -0,0 +1,29 @@
+/**
+ * Hello world!
+ */
+public class App {
+    /**
+     * Main method - entry point of the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    /**
+     * Returns a greeting message
+     * @param name the name to greet
+     * @return greeting message
+     */
+    public String getGreeting(String name) {
+        return "Hello, " + name + "!";
+    }
+    
+    /**
+     * Returns the current version of the application
+     * @return version string
+     */
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
